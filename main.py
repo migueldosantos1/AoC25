b123f151c3c0ad207164f9161abad9320834e4a3
migueldosantos1 <miguelhcs.2005@gmail.com>
2025-12-17 16:23:09 +0000

    day 1 part 2

--- main.py
+++ main.py
@@ -4,20 +4,20 @@ position = 50
 counter = 0
 
 for line in f:
-    if line[0] == "L":
-        value = int(line[1:])
-        position -= value
-        while position < 0:
-            left = position + 100
-            position = left
-    else:
-        value = int(line[1:])
-        position += value
-        while position >= 100:
-            left = position - 100
-            position = left
-    if position == 0:
-        counter += 1
+    direction = line[0]
+    value = int(line[1:])
+
+    for _ in range(value):
+        if direction == "L":
+            position -= 1
+        else:
+            position += 1
+        if position < 0:
+            position = 99
+        elif position > 99:
+            position = 0
+        if position == 0:
+            counter += 1
 
 print("Posição:", position)
 print("Resposta:", counter)
