d088aaedfeda71b4befe44a9687c4ff584ab75a1
migueldosantos1 <miguelhcs.2005@gmail.com>
2026-01-27 22:03:40 +0000

    última mudança, cansei-me do advent of xmas

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 with open("input2.txt", "r") as f:
     content = f.read().strip()
 
+i = 0
 sum = 0
 pairs = content.split(",")
 
@@ -8,12 +9,14 @@ for pair in pairs:
     begin, end = pair.split("-")
     for number in range(int(begin), int(end) + 1):
         s = str(number)
-        lenght = len(s) 
-        mid = lenght // 2
-        if(lenght % 2 == 0):
-            esq = s[:mid]
-            dir = s[mid:]
-            if(esq == dir):
+        length = len(s)
+        for number in range(1, length // 2 + 1):
+            if(number == number[i + 1]):
                 sum += number
+            elif(number[i] == number[i + 1] and number[i + 2] == number[i + 3]):
+                sum += number
+            elif(number[i] == number[i + 1] and number[i + 2] == number[i + 3] and number[i + 4] == number[i + 5]):
+                sum += number
+            i += 1
 
 print(sum)
